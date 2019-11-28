028401e5c19e17ed63f9d7f01adb1a5da3ddbf6d
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-11-28 12:17:44 +0100

    Add other fields such as username, isAdmin, isSuper etc. to the

--- src/apiV1/users/user.model.ts
+++ src/apiV1/users/user.model.ts
@@ -1,14 +1,26 @@
-import * as mongoose from "mongoose";
-const Schema = mongoose.Schema;
+// import * as mongoose from "mongoose";
+// const Schema = mongoose.Schema;
+import mongoose from "mongoose";
+import { UserType } from "./user.typings";
+import uuid4 from "uuid/v4";
 
-const UserSchema = Schema(
+export interface UserSchema extends UserType, mongoose.Document {}
+const User = new mongoose.Schema(
   {
-    name: {
+    userId: {
+      type: String
+    },
+
+    firstName: {
       type: String,
       required: true,
       trim: true
     },
     lastName: {
+      type: String,
+      required: true
+    },
+    username: {
       type: String,
       required: true
     },
@@ -23,12 +35,39 @@ const UserSchema = Schema(
       type: String,
       required: true,
       trim: true
+    },
+    isAdmin: {
+      type: Boolean
+    },
+
+    isSuper: {
+      type: Boolean
+    },
+
+    deleted: {
+      type: Boolean
+    },
+    salt: {
+      type: String
+    },
+    hash: {
+      type: String
+    },
+    token: {
+      type: String
     }
   },
   {
+    id: false,
     timestamps: true,
     useNestedStrict: true
   }
 );
 
-export default mongoose.model("User", UserSchema);
+User.pre<UserSchema>("save", function() {
+  if (this.isNew) {
+    this.userId = uuid4();
+  }
+});
+
+export default mongoose.model("User", User);
