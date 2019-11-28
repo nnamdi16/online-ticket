67860aedb3dbb8a67087ecb0d9344d69ac7859a1
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-11-28 12:18:15 +0100

    Add useUnifiedTopology to mongoose client

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,17 +1,17 @@
-import * as mongoose from 'mongoose';
-import CONFIG from './config';
+import mongoose from "mongoose";
+import CONFIG from "./config";
 
-mongoose.set('useCreateIndex', true);
+mongoose.set("useCreateIndex", true);
 
 // Connecting to the database
 export default (async () => {
   try {
-    await mongoose.connect(
-      CONFIG.DB_HOST,
-      { useNewUrlParser: true }
-    );
+    await mongoose.connect(CONFIG.DB_HOST, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
     // listen for requests
-    console.log('The Conection is Ok');
+    console.log("The Conection is Ok");
   } catch (err) {
     console.log(`${err} Could not Connect to the Database. Exiting Now...`);
     process.exit();
