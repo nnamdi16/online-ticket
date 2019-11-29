54083f01471664fb7241a2ecae3f2f02ebfb1f40
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-11-29 04:15:38 +0100

    Create the signUp endpoint and validations

--- src/apiV1/users/user.route.ts
+++ src/apiV1/users/user.route.ts
@@ -1,23 +1,51 @@
-import { Router } from 'express';
-import verifyToken from '../../helpers/verifyToken';
-import Controller from './user.controller';
+import { Router, Response, Request, NextFunction } from "express";
+// import verifyToken from '../../helpers/verifyToken';
+import Controller from "./user.controller";
+import { validateUser as validate } from "./user.validation";
 
-const user: Router = Router();
+const users: Router = Router();
+
 const controller = new Controller();
 
 // Create a user
-user.post('/');
+users.post(
+  "/signUp",
+  async (req: Request | any, res: Response, next: NextFunction) => {
+    console.log("We are here");
+    try {
+      const { error, value } = validate(req.body);
+      console.log(error, value);
+      if (error) {
+        return res.status(400).json({
+          message: error.message
+        });
+      }
+      const data = await controller.createUser(value);
+      if (data.error) {
+        res.status(401).json({
+          message: data.msg
+        });
+        return;
+      }
+      const { _id, firstName, lastName, username, email } = data.user;
+      res.status(200).json({ _id, firstName, lastName, username, email });
+      return res.json;
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
-// Retrieve all Users
-user.get('/', controller.findAll);
+// // Retrieve all Users
+// user.get('/', controller.findAll);
 
-// Retrieve a Specific User
-user.get('/:id', verifyToken, controller.findOne);
+// // Retrieve a Specific User
+// user.get('/:id', verifyToken, controller.findOne);
 
-// Update a User with Id
-user.put('/:id', controller.update);
+// // Update a User with Id
+// user.put('/:id', controller.update);
 
-// Delete a User with Id
-user.delete('/:id', controller.remove);
+// // Delete a User with Id
+// user.delete('/:id', controller.remove);
 
-export default user;
+export default users;
