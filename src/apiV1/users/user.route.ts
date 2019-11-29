8918e32efaeb11e5a22a1e4e8e371b444c01ef00
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-11-29 08:08:47 +0100

    Remove zombie code and created the standard response message

--- src/apiV1/users/user.route.ts
+++ src/apiV1/users/user.route.ts
@@ -14,24 +14,36 @@ users.post(
     console.log("We are here");
     try {
       const { error, value } = validate(req.body);
-      console.log(error, value);
       if (error) {
         return res.status(400).json({
+          success: false,
           message: error.message
         });
       }
       const data = await controller.createUser(value);
       if (data.error) {
         res.status(401).json({
+          success: false,
           message: data.msg
         });
         return;
       }
-      const { _id, firstName, lastName, username, email } = data.user;
-      res.status(200).json({ _id, firstName, lastName, username, email });
-      return res.json;
+      const { userId, firstName, lastName, username, email } = data.user;
+      const { token } = data.user;
+      const userDetails = { userId, firstName, lastName, username, email };
+      return res.status(201).json({
+        success: true,
+        message: `${username} Successfully created`,
+        data: userDetails,
+        status: 201,
+        token
+      });
     } catch (error) {
-      next(error);
+      // next(error);
+      res.status(500).json({
+        success: false,
+        message: error.toString()
+      });
     }
   }
 );
