ffe99883d4ad25ce17eb468dff6b7411e5c72d09
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-12-03 13:37:06 +0100

    Create ticket typings and models

---
+++ src/apiV1/ticket/ticket.model.ts
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+import uuid4 from "uuid/v4";
+import { Ticket } from "./ticket.typings";
+
+export interface TicketSchema extends Ticket, mongoose.Document {}
+
+const Ticket = new mongoose.Schema(
+  {
+    ticketId: {
+      type: String
+    },
+
+    ticketTypeId: {
+      type: String,
+      required: true
+    },
+    amount: {
+      type: String,
+      required: true
+    },
+    userId: {
+      type: Boolean
+    }
+  },
+  {
+    id: false,
+    timestamps: true
+  }
+);
+
+Ticket.pre<TicketSchema>("save", function() {
+  if (this.isNew) {
+    this.ticketId = uuid4();
+  }
+});
+
+export default mongoose.model<TicketSchema>("Ticket", Ticket);
