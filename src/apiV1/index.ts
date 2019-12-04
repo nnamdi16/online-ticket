85b63ef11d31fe69b1a931b0ba393703f1030c53
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-12-04 16:15:33 +0100

    Create a new resource or document called event

--- src/apiV1/index.ts
+++ src/apiV1/index.ts
@@ -1,10 +1,12 @@
-import { Router } from 'express';
+import { Router } from "express";
 // import auth from './auth/auth.route';
-import users from './users/user.route';
+import users from "./users/user.route";
+import event from "./event/event.route";
 
 const router: Router = Router();
 
 // router.use('/', auth);
-router.use('/users', users);
+router.use("/users", users);
+router.use("/events", event);
 
 export default router;
