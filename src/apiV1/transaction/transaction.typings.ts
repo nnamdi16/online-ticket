5faf899fc44f7f0dba06ca5236a4cb93add0c052
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-12-05 11:21:03 +0100

    Modified transaction typings

--- src/apiV1/transaction/transaction.typings.ts
+++ src/apiV1/transaction/transaction.typings.ts
@@ -3,9 +3,9 @@ import { Document } from "mongoose";
 export interface TransactionType extends Document {
   eventId: string;
   ticketId: string;
-  amount: string;
-  userId:string;
-  transactionRef: number;
-  status:string
+  amount: number;
+  userId: string;
+  transactionRef: string;
+  status: string;
   [key: string]: any;
 }
