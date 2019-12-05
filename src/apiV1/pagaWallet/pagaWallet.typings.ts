0777a0db09f844dfc8bf0ecfd87327bc58f51a1f
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-12-05 11:09:26 +0100

    Change the naming of these files

--- src/apiV1/pagaWallet/pagaWallet.typings.ts
+++
@@ -1,9 +0,0 @@
-import { Document } from "mongoose";
-
-export interface PagaWallet extends Document {
-  deleted: boolean;
-  userId: string;
-  pagaReferenceKey: string;
-  amount: number;
-  [key: string]: any;
-}
