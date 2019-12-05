35ca9e7e380067166f4e7a98d063779c443531ed
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-12-05 11:24:02 +0100

    Modified the user controller

--- src/apiV1/users/user.controller.ts
+++ src/apiV1/users/user.controller.ts
@@ -1,178 +1,114 @@
-// import * as bcrypt from "bcrypt";
-// import { Request, Response } from "express";
-// import * as jwt from "jwt-then";
-// import config from "../../config/config";
-import User, { UserSchema } from "./user.model";
-import { UserType } from "./user.typings";
+import { Response, Request, NextFunction } from "express";
+import UserService from "./user.services";
+import { validateUser as validate } from "./user.validation";
 import jwt from "jsonwebtoken";
+import config from "../../config/config";
 
+const services = new UserService();
+
+// Create a user
 export default class UserController {
-  public register = async (data: UserSchema): Promise<any> => {
-    const newUser = new User({
-      firstName: data.firstName,
-      lastName: data.lastName,
-      username: data.username,
-      email: data.email,
-      password: data.password,
-      deleted: data.deleted,
-      isPlanner: data.isPlanner
-    });
-
+  public register = async (
+    req: Request | any,
+    res: Response,
+    next: NextFunction
+  ) => {
     try {
-      const validUser = await User.exists({
-        email: data.email
-      });
-      if (validUser) {
-        return {
-          error: true,
-          msg: "User email already exist"
-        };
+      const { error, value } = validate(req.body);
+      console.log("We are here o");
+      if (error) {
+        return res.status(400).json({
+          success: false,
+          message: error.message
+        });
       }
-      newUser.setPassword(data.password);
-      newUser.generateJWT();
-      const user = await newUser.save();
-      return {
-        error: false,
-        user
-      };
+      const data = await services.register(value);
+
+      if (data.error) {
+        return res.status(401).json({
+          success: false,
+          message: data.msg
+        });
+      }
+
+      const { userId, firstName, lastName, username, email } = data.user;
+      const { token } = data.user;
+      const userDetails = { userId, firstName, lastName, username, email };
+      return res.status(201).json({
+        success: true,
+        message: `${username} Successfully created`,
+        data: userDetails,
+        status: 201,
+        token
+      });
     } catch (error) {
-      throw new Error(error);
+      res.status(500).json({
+        success: false,
+        message: error.toString(),
+        status: 500
+      });
+      next(error);
     }
   };
 
-  public authenticate = async (data: UserSchema): Promise<any> => {
-    const newUser = new User();
-    const user = await User.findOne({ email: data.email });
-    if (!user) {
-      return {
-        error: true,
-        msg: "Invalid Email or Password"
-      };
-    }
-    const { password, salt } = user;
-    const matchedPassword = await newUser.comparePassword(
-      data.password,
-      salt,
-      password
-    );
+  public login = async (
+    req: Request | any,
+    res: Response | any,
+    next: NextFunction
+  ) => {
+    try {
+      const { error, value } = validate(req.body);
+      console.log(value);
+      if (error) {
+        res.status(400).json({
+          success: false,
+          message: error.message
+        });
+        return;
+      }
 
-    if (!matchedPassword) {
-      return {
-        error: true,
-        msg: "Invalid Credentials. Please check your email and password"
-      };
-    }
+      const data = await services.authenticate(value);
 
-    return {
-      error: false,
-      user
-    };
+      console.log(data);
+      const { userId, email } = data.user;
+      // req.session.user = { id: userId, email };
+      if (data.error) {
+        res.status(401).json({
+          message: data.msg
+        });
+        return;
+      }
+
+      const token = await jwt.sign({ email }, config.JWT_ENCRYPTION, {
+        expiresIn: config.JWT_EXPIRATION
+      });
+
+      // Valid login
+
+      res.status(200).json({
+        id: userId,
+        email,
+        // sessionID: req.sessionID,
+        data: token
+      });
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        message: error.message
+      });
+      next(error);
+    }
   };
 }
 
-// export default class UserController {
+// // Retrieve all Users
+// user.get('/', controller.findAll);
 
-//   public findAll = async (req: Request, res: Response): Promise<any> => {
-//     try {
-//       const users = await User.find();
-//       if (!users) {
-//         return res.status(404).send({
-//           success: false,
-//           message: 'Users not found',
-//           data: null
-//         });
-//       }
+// // Retrieve a Specific User
+// user.get('/:id', verifyToken, controller.findOne);
 
-//       res.status(200).send({
-//         success: true,
-//         data: users
-//       });
-//     } catch (err) {
-//       res.status(500).send({
-//         success: false,
-//         message: err.toString(),
-//         data: null
-//       });
-//     }
-//   };
+// // Update a User with Id
+// user.put('/:id', controller.update);
 
-//   public findOne = async (req: Request, res: Response): Promise<any> => {
-//     try {
-//       const user = await User.findById(req.params.id, { password: 0 });
-//       if (!user) {
-//         return res.status(404).send({
-//           success: false,
-//           message: 'User not found',
-//           data: null
-//         });
-//       }
-
-//       res.status(200).send({
-//         success: true,
-//         data: user
-//       });
-//     } catch (err) {
-//       res.status(500).send({
-//         success: false,
-//         message: err.toString(),
-//         data: null
-//       });
-//     }
-//   };
-
-//   public update = async (req: Request, res: Response): Promise<any> => {
-//     const { name, lastName, email, password } = req.body;
-//     try {
-//       const userUpdated = await User.findByIdAndUpdate(
-//         req.params.id,
-//         {
-//           $set: {
-//             name,
-//             lastName,
-//             email,
-//             password
-//           }
-//         },
-//         { new: true }
-//       );
-//       if (!userUpdated) {
-//         return res.status(404).send({
-//           success: false,
-//           message: 'User not found',
-//           data: null
-//         });
-//       }
-//       res.status(200).send({
-//         success: true,
-//         data: userUpdated
-//       });
-//     } catch (err) {
-//       res.status(500).send({
-//         success: false,
-//         message: err.toString(),
-//         data: null
-//       });
-//     }
-//   };
-
-//   public remove = async (req: Request, res: Response): Promise<any> => {
-//     try {
-//       const user = await User.findByIdAndRemove(req.params.id);
-
-//       if (!user) {
-//         return res.status(404).send({
-//           success: false,
-//           message: 'User not found',
-//           data: null
-//         });
-//       }
-//       res.status(204).send();
-//     } catch (err) {
-//       res.status(500).send({
-//         success: false,
-//         message: err.toString(),
-//         data: null
-//       });
-//     }
-//   };
-// }
+// // Delete a User with Id
+// user.delete('/:id', controller.remove);
