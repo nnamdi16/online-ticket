e0fec658de6a614fc742d638c6f83b8b02dd7b03
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-12-05 11:07:24 +0100

    import ticket resource

--- src/apiV1/index.ts
+++ src/apiV1/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 // import auth from './auth/auth.route';
 import users from "./users/user.route";
 import event from "./event/event.route";
+import ticket from "./ticket/ticket.route";
 
 const router: Router = Router();
 
 // router.use('/', auth);
 router.use("/users", users);
 router.use("/events", event);
+router.use("/tickets", ticket);
 
 export default router;
