24444515f11b9a3f1298c946a2d376636b676582
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-12-09 00:17:42 +0100

    Add transaction route

--- src/apiV1/index.ts
+++ src/apiV1/index.ts
@@ -3,12 +3,13 @@ import { Router } from "express";
 import users from "./users/user.route";
 import event from "./event/event.route";
 import ticketType from "./ticketType/ticketType.route";
-
+import transaction from "./transaction/transaction.route";
 const router: Router = Router();
 
 // router.use('/', auth);
 router.use("/users", users);
 router.use("/events", event);
 router.use("/ticketType", ticketType);
+router.use("/pay", transaction);
 
 export default router;
