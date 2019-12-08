2be3274e6f0793976beaf6aab42254fb97b41256
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-12-09 00:48:42 +0100

    Create methods in TransactionService

--- src/apiV1/transaction/transaction.service.ts
+++ src/apiV1/transaction/transaction.service.ts
@@ -3,31 +3,126 @@ import Ticket, { TicketSchema } from "../ticket/ticket.model";
 import OnlineTicketWallet, {
   OnlineTicketWalletSchema
 } from "../onlineTicketWallet/onlineTicketWallet.model";
-// import pagaBusinessClient = require("paga-business");
+
 import PagaBusiness from "./pagaBuildRequest";
-// import pagaBusinessClient = require("../../../node_modules/paga-business/PagaBusinessClient.js");
+
 export default class TransactionService {
-  public transactionSetup = async (data: any):Promise<any> => {
+  public getMobileOperators = async (obj: any): Promise<any> => {
+    const pagaBusinessClient = new PagaBusiness();
     try {
-      const transaction =  await PagaBusiness.airtimePurchase()
-    } catch (error) {}
+      const { referenceNumber, locale } = obj;
+      const parameters: string[] = [referenceNumber, locale];
+      const mobileOperators = await pagaBusinessClient.pagaBusinessClient.getMobileOperators(
+        ...parameters
+      );
+      const { data } = mobileOperators;
+      return {
+        error: false,
+        data
+      };
+    } catch (error) {
+      return {
+        error: true,
+        data: error.message
+      };
+    }
   };
-  public initiateTransaction = async (
-    data: TransactionSchema
-  ): Promise<any> => {
-    const newTransaction = new Transaction({
-      eventId: data.eventId,
-      amount: data.amount,
-      userId: data.userId,
-      transactionRef: data.transactionRef,
-      onlineTicketWalletId: data.onlineTicketWalletId
-    });
+
+  public initiateTransaction = async (data: any): Promise<any> => {
+    const pagaBusinessClient = new PagaBusiness();
+    const {
+      // eventId,
+      // amount,
+      // userId,
+      // ticketId,
+      // transactionRef,
+      referenceNumber,
+      amount,
+      currency,
+      destinationAccount,
+      destinationBank,
+      withdrawalCode,
+      sourceOfFunds,
+      transferReference,
+      senderPrincipal,
+      senderCredentials,
+      suppressRecipientMsg,
+      locale,
+      alternateSenderName,
+      minRecipientKYCLevel,
+      holdingPeriod
+    } = data;
+    // const newTransaction = new Transaction({
+    //   eventId,
+    //   amount,
+    //   userId,
+    //   ticketId,
+    //   transactionRef
+    // });
 
     try {
-      const getWalletBalance = await OnlineTicketWallet.findOne({
-        onlineTicketWalletId: data.onlineTicketWalletId
-      });
-      console.log(getWalletBalance);
+      // const {
+      //   referenceNumber,
+      //   currency,
+      //   destinationAccount,
+      //   destinationBank,
+      //   senderPrincipal,
+      //   senderCredentials,
+      //   withdrawalCode,
+      //   sourceOfFunds,
+      //   transferReference,
+      //   suppressRecipientMsg,
+      //   locale,
+      //   alternateSenderName,
+      //   minRecipientKYCLevel,
+      //   holdingPeriod
+      // } =
+
+      // const userDetails = [
+      //   referenceNumber,
+      //   amount,
+      //   currency,
+      //   destinationAccount,
+      //   destinationBank,
+      //   withdrawalCode,
+      //   sourceOfFunds,
+      //   transferReference,
+      //   suppressRecipientMsg,
+      //   locale,
+      //   alternateSenderName,
+      //   minRecipientKYCLevel,
+      //   holdingPeriod
+      // ];
+      const transferFunds = await pagaBusinessClient.pagaBusinessClient.moneyTransfer(
+        referenceNumber,
+        amount,
+        currency,
+        destinationAccount,
+        destinationBank,
+        senderPrincipal,
+        senderCredentials,
+        withdrawalCode,
+        sourceOfFunds,
+        transferReference,
+        suppressRecipientMsg,
+        locale,
+        alternateSenderName,
+        minRecipientKYCLevel,
+        holdingPeriod
+      );
+
+      console.log(transferFunds);
+      console.log("Grow boy");
+
+      return {
+        error: false,
+        data: transferFunds.data
+      };
+
+      // const getWalletBalance = await OnlineTicketWallet.findOne({
+      //   onlineTicketWalletId: data.onlineTicketWalletId
+      // });
+      // console.log(getWalletBalance);
     } catch (error) {
       throw new Error(error);
     }
