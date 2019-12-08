7516f1fc32cfe54ec8e2f3d4124711ea07c96375
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-12-08 10:51:33 +0100

    Changed the import from ticket to ticketType

--- src/apiV1/index.ts
+++ src/apiV1/index.ts
@@ -2,13 +2,13 @@ import { Router } from "express";
 // import auth from './auth/auth.route';
 import users from "./users/user.route";
 import event from "./event/event.route";
-import ticket from "./ticket/ticket.route";
+import ticketType from "./ticketType/ticketType.route";
 
 const router: Router = Router();
 
 // router.use('/', auth);
 router.use("/users", users);
 router.use("/events", event);
-router.use("/tickets", ticket);
+router.use("/ticketType", ticketType);
 
 export default router;
