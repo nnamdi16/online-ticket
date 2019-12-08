98dd8e0703b1342c20755428ece7c282d6c7dfd2
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-12-09 00:33:36 +0100

    Create Transaction controller to carry out moneyTransfer and get mobile operator

--- src/apiV1/transaction/transaction.controller.ts
+++ src/apiV1/transaction/transaction.controller.ts
@@ -3,3 +3,49 @@ import TransactionService from "../transaction/transaction.service";
 import { validateTransaction as validate } from "../transaction/transaction.validation";
 import jwt from "jsonwebtoken";
 import config from "../../config/config";
+
+const service = new TransactionService();
+
+export default class TransactionController {
+  public moneyTransfer = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      const data = await service.initiateTransaction(req.body);
+      return res.status(201).json({
+        success: true,
+        message: "Transaction completed",
+        data
+      });
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        message: error.message
+      });
+      return next(error);
+    }
+  };
+
+  public getMobileOperators = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      const data = await service.getMobileOperators(req.body);
+      return res.status(201).json({
+        success: true,
+        message: "Transaction completed",
+        data
+      });
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        message: error.message
+      });
+      return next(error);
+    }
+  };
+}
