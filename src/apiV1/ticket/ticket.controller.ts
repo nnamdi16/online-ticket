7ed841e3a614f58077b630a786766461ce8359c1
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-12-12 23:18:33 +0100

    Create methods for ticketControllet, such as create event, get all events  etc.

--- src/apiV1/ticket/ticket.controller.ts
+++ src/apiV1/ticket/ticket.controller.ts
@@ -1,93 +1,375 @@
-import Ticket, { TicketSchema } from "./ticket.model";
-import { TicketType } from "../ticketType/ticketType.typings";
-
-// const model = {
-//   title,
-//   description,
-//   location,
-//   category,
-//   startDate,
-//   endDate,
-//   time,
-//   noOfAttendees,
-//   eventUrl,
-//   imageUrl,
-//   authorId
-// };
+import { Router, Response, Request, NextFunction } from "express";
+// import verifyToken from '../../helpers/verifyToken';
+import TicketService from "./ticket.service";
+import { validateTicket as validate } from "./ticket.validation";
+import jwt from "jsonwebtoken";
+import config from "../../config/config";
+import { object } from "@hapi/joi";
+import { TicketSchema } from "./ticket.model";
+const event: Router = Router();
+const services = new TicketService();
 
 export default class TicketController {
-  createTicket = async (data: TicketType): Promise<any> => {
-    
-    const newTicket = new Ticket({
-      ticketTypeId: data.ticketTypeId,
-      amount: data.amount,
-      userId: data.userId
-    });
+  /**
+   * Create an Event
+   */
+  public createTicket = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
     try {
-      const validEvent = await Ticket.exists({ title: data.title });
-      console.log(validEvent);
-      if (validEvent) {
-        return {
-          error: true,
-          msg: " Event has already been created"
-        };
+      const { error, value } = validate(req.body);
+      console.log(value);
+      if (error) {
+        res.status(400).json({
+          success: false,
+          message: error.message
+        });
+        next(error);
+        return;
       }
-      const event = await newTicket.save();
-      return {
-        error: false,
-        event
-      };
+      console.log(value);
+      const data = await services.createTicket(value);
+      if (data.error) {
+        res.status(201).json({
+          success: false,
+          message: data.msg
+        });
+        next(error);
+        return;
+      }
+      console.log(data);
+      const ticketData = await data.ticket;
+      return res.status(201).json({
+        success: true,
+        message: "Successfully created ticket",
+        data: ticketData,
+        status: 201
+      });
     } catch (error) {
-      throw new Error(error);
+      res.status(500).json({
+        success: false,
+        message: error.message,
+        status: 500
+      });
+      next(error);
+      return;
     }
   };
 
-  getAllTickets = async (callback): Promise<any> => {
-    await Ticket.find({}, (err: any, res: any) => {
-      if (err) {
-        return callback(err);
-      } else {
-        callback(err, res);
-      }
-    });
-  };
+  /**
+   * Get all events
+   */
 
-  getOneTicketById = async (id, callback): Promise<any> => {
-    await Ticket.findOne({ ticketId: id }, (err: any, res: any) => {
-      if (err) {
-        return callback(err);
-      } else {
-        callback(err, res);
-      }
-    });
-  };
-
-  updateTicket = async (ticketId, amount, callback): Promise<any> => {
-    await Ticket.findOneAndUpdate(
-      {
-        ticketId
-      },
-      {
-        $set: {
-          amount
-        }
-      },
-      { upsert: true, new: true },
-      (err, res) => {
+  public getAllTickets = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      await services.getAllTickets((err, data) => {
         if (err) {
-          return callback(err);
+          res.status(400).json({
+            success: false,
+            error: err.message
+          });
+          next(err);
+          return;
         }
-        callback(err, res);
-      }
-    );
+        return res.status(200).json({
+          success: true,
+          message: data
+        });
+      });
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        message: error.message,
+        status: 500
+      });
+      next(error);
+      return;
+    }
   };
 
-  deleteTicket = async (id, callback) => {
-    Ticket.findOneAndDelete({ eventId: id }, (err, res: TicketSchema) => {
-      if (err) {
-        return callback(err);
-      }
-      return callback(err, res);
-    });
+  /**
+   * Get single event by Id
+   */
+  public getOneTicketById = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    const id = req.params.id;
+    try {
+      await services.getOneTicketById(id, (err, data) => {
+        if (err) {
+          res.status(404).json({
+            success: false,
+            error: err.message
+          });
+          next(err);
+          return;
+        }
+        return res.status(200).json({
+          success: true,
+          message: data
+        });
+      });
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        message: error.message,
+        status: 500
+      });
+      next(error);
+      return;
+    }
+  };
+
+  /**
+   * Update event
+   */
+  public updateTicket = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    const id = req.params.id;
+    try {
+      await services.updateTicket(
+        id,
+        req.body.amount,
+
+        (err, data) => {
+          if (err) {
+            res.status(400).json({
+              success: false,
+              error: err.message,
+              status: 400
+            });
+            next(err);
+            return;
+          }
+          return res.status(200).json({
+            success: true,
+            message: data,
+            status: 200
+          });
+        }
+      );
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        message: error.message,
+        status: 500
+      });
+      next(error);
+      return;
+    }
+  };
+
+  /**
+   * Delete ticket
+   */
+  public deleteTicket = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    const ticketId = req.params.id;
+    try {
+      await services.deleteTicket(ticketId, (err, data: TicketSchema) => {
+        if (err) {
+          return res.status(404).json({
+            success: false,
+            message: err.message
+          });
+        }
+        return res.status(200).json({
+          status: ` Successfully deleted`
+        });
+      });
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        message: error.message
+      });
+      return next(error);
+    }
   };
 }
+
+/**
+ * Create an Event
+ */
+
+// event.post(
+//   "/create",
+//   async (req: Request, res: Response, next: NextFunction) => {
+//     try {
+//       const { error, value } = validate(req.body);
+//       if (error) {
+//         res.status(400).json({
+//           success: false,
+//           message: error.message
+//         });
+//         next(error);
+//         return;
+//       }
+//       const data = await services.createTicket(value);
+//       if (data.error) {
+//         res.status(201).json({
+//           success: false,
+//           message: data.msg
+//         });
+//         next(error);
+//         return;
+//       }
+//       const ticketData = await data.ticket;
+//       return res.status(201).json({
+//         success: true,
+//         message: "Successfully created ticket",
+//         data: ticketData,
+//         status: 201
+//       });
+//     } catch (error) {
+//       res.status(500).json({
+//         success: false,
+//         message: error.message,
+//         status: 500
+//       });
+//       next(error);
+//       return;
+//     }
+//   }
+// );
+
+/**
+ * Get all events
+ */
+event.get("/", async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    await services.getAllTickets((err, data) => {
+      if (err) {
+        res.status(400).json({
+          success: false,
+          error: err.message
+        });
+        next(err);
+        return;
+      }
+      return res.status(200).json({
+        success: true,
+        message: data
+      });
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      status: 500
+    });
+    next(error);
+    return;
+  }
+});
+
+/**
+ * Get single event by Id
+ */
+
+event.get("/:id", async (req: Request, res: Response, next: NextFunction) => {
+  const id = req.params.id;
+  try {
+    await services.getOneTicketById(id, (err, data) => {
+      if (err) {
+        res.status(404).json({
+          success: false,
+          error: err.message
+        });
+        next(err);
+        return;
+      }
+      return res.status(200).json({
+        success: true,
+        message: data
+      });
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      status: 500
+    });
+    next(error);
+    return;
+  }
+});
+
+/**
+ * Update event
+ */
+event.put("/:id", async (req: Request, res: Response, next: NextFunction) => {
+  const id = req.params.id;
+  try {
+    await services.updateTicket(
+      id,
+      req.body.amount,
+
+      (err, data) => {
+        if (err) {
+          res.status(400).json({
+            success: false,
+            error: err.message,
+            status: 400
+          });
+          next(err);
+          return;
+        }
+        return res.status(200).json({
+          success: true,
+          message: data,
+          status: 200
+        });
+      }
+    );
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      status: 500
+    });
+    next(error);
+    return;
+  }
+});
+
+event.delete(
+  "/:id",
+  async (req: Request, res: Response, next: NextFunction) => {
+    const ticketId = req.params.id;
+    try {
+      await services.deleteTicket(ticketId, (err, data: TicketSchema) => {
+        if (err) {
+          return res.status(404).json({
+            success: false,
+            message: err.message
+          });
+        }
+        return res.status(200).json({
+          status: ` Successfully deleted`
+        });
+      });
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        message: error.message
+      });
+      return next(error);
+    }
+  }
+);
+
+// export default event;
