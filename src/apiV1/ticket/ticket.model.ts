d0023c9fb5f63267c4aac1b8a2c606fa46ff73c4
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-12-12 23:19:38 +0100

    Modified ticket model schema and it's referencing from ObjectId referencing to model referencing

--- src/apiV1/ticket/ticket.model.ts
+++ src/apiV1/ticket/ticket.model.ts
@@ -1,37 +1,50 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
 import uuid4 from "uuid/v4";
-import { TicketType } from "./ticket.typings";
+import uuid from "uuid";
+import { Ticket } from "./ticket.typings";
 
-export interface TicketSchema extends TicketType, mongoose.Document {}
+export interface TicketSchema extends Ticket, mongoose.Document {}
 
 const Ticket = new mongoose.Schema(
   {
-    ticketId: {
-      type: String
+    // ticketId: {
+    //   type: String
+    // },
+
+    _id: {
+      type: String,
+      default: uuid4
     },
 
     ticketTypeId: {
       type: String,
+      ref: "TicketType",
       required: true
     },
-    amount: {
-      type: String,
+    price: {
+      type: Number,
       required: true
     },
     userId: {
-      type: Boolean
+      type: String,
+      ref: "User",
+      required: true
+    },
+    numberOfTickets: {
+      type: Number,
+      required: true
     }
   },
+
   {
-    id: false,
     timestamps: true
   }
 );
 
-Ticket.pre<TicketSchema>("save", function() {
-  if (this.isNew) {
-    this.ticketId = uuid4();
-  }
-});
+// Ticket.pre<TicketSchema>("save", function() {
+//   if (this.isNew) {
+//     this.ticketId = uuid4();
+//   }
+// });
 
 export default mongoose.model<TicketSchema>("Ticket", Ticket);
